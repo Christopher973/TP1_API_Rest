09fe665e0bb8066eca5460bd9dbe5f9a97cf053c
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-10-22 15:34:30 +0200

    edit

--- swagger.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("vendor/autoload.php");
-
-$openapi = \OpenApi\Generator::scan([__DIR__ . '/api', __DIR__ . '/modele']);
-
-header('Content-Type: application/x-yaml');
-echo $openapi->toYaml();
